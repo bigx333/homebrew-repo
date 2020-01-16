538ed094f1ccfc56839ac429adc6e0e0211f1e22
Nelson Luiz Ferraz de Camargo <ncamargo@othostelecom.co.za>
2020-01-16 19:00:05 +0200

    Removed bottle

--- Formula/profanity-omemo.rb
+++ Formula/profanity-omemo.rb
@@ -4,12 +4,6 @@ class ProfanityOmemo < Formula
     url "https://profanity-im.github.io/profanity-0.7.1.tar.gz"
     sha256 "3fe442948ff2ee258681c3812e878d39179dcf92e1c67bc8fe0ef8896440b05b"
 
-    bottle do
-      sha256 "e758626095ed29ecb2165ed66fbd94ff33208bc021734c9b6da8ad07cc8ba9ef" => :catalina
-      sha256 "635da190c3390f3afbb8cd950cf325904b18a139ed3326950b101f1ba29c0cee" => :mojave
-      sha256 "00c708f8f2061059b919b9033775730ea0587671d5e1d0a45214de3599f676ed" => :high_sierra
-    end
-
     head do
       url "https://github.com/boothj5/profanity.git"
 
